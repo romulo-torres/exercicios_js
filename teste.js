b84b647264265777fe9373c8290ffdb39a99fb59
romulo-torres <romulotorres14@gmail.com>
2026-04-29 02:18:05 -0400

    exercicio 1 do slide do node.js

---
+++ teste.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs')
+
+const server = http.createServer(function(req,res){
+    let dir = process.argv.at(2);
+    res.writeHead(200,{"content-type":"text/html;charset=utf-8"});
+    if(dir){
+        let arquivos = fs.readdir(dir,(err, arquivos) => {
+            if (err) {
+                res.write("Erro ao ler diretório");
+                return res.end();
+            }
+        arquivos.forEach(element => {
+            res.write(element + "<br>")
+        });
+        res.end();
+        });
+    }
+    else{
+        res.write("Error: diretório inexistente")
+        res.end();
+    }
+});
+
+
+server.listen(3333);
